0d72f567970d214c49449e5ebef631d4c8f4d4b8
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2024-12-05 12:14:37 +0100

    Finished Exercici 2 Closes #3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Prova;
+
+namespace Exercici1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            const string InsertaValor = "Inserta el valor que vulguis buscar a l'array:";
+            const string Error = "El valor insertat ha de ser un numero enter";
+            const string Trobat = "El teu numero si esta a l'Array";
+            const string NoTrobat = "El teu numero no esta a l'Array";
+            const string ErrorArray = "La longitud del array es invalida";
+            int numUser;
+            int resultat;
+            int[] arrayBuscar = { 10, -4, 6, 4, 8, 13, 2, -4 };
+            SecondSort.Order(arrayBuscar, 0, arrayBuscar.Length - 1);
+            Console.WriteLine(InsertaValor);
+            try
+            {
+                numUser = int.Parse(Console.ReadLine());
+                resultat = SearchClass.BinarySearch(arrayBuscar, 0, arrayBuscar.Length - 1, numUser);
+                if (resultat == -1)
+                {
+                    Console.WriteLine(NoTrobat);
+                }
+                else
+                {
+                    Console.WriteLine(Trobat);
+                }
+            }
+            catch (FormatException) { 
+                Console.WriteLine(Error);
+            }
+        }
+    }
+}
